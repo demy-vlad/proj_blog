f4e1b4db30af1ce3f6cdae491912d0320e57ac22
Server_WIN_10 <you@example.com>
2024-04-24 15:43:27 +0300

    added blog

--- blog/main/views.py
+++ blog/main/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 from .catalog_entry_counts import count_blog_entries_by_catalog  
 from .latest_publications import latest_publications
-    
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
+   
 def index(request):
     return render(request, 'index.html', {
         'catalogs': count_blog_entries_by_catalog(),
         'latest_publications': latest_publications(),
         })
 
-from django.shortcuts import render, get_object_or_404
-from .models import Blog
-
 def blog_detail(request, pk):
     blog = get_object_or_404(Blog, pk=pk)
-    return render(request, 'blog_detail.html', {'blog': blog})
+    return render(request, 'blog_detail.html', {
+        'blog': blog,
+        'catalogs': count_blog_entries_by_catalog(),
+        'latest_publications': latest_publications(),
+        })
