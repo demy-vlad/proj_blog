246539f76049c5d0d0914ac55cc87562a158ba51
Server_WIN_10 <you@example.com>
2024-04-25 15:23:11 +0300

    update

--- blog/main/views.py
+++ blog/main/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .catalog_entry_counts import count_blog_entries_by_catalog  
-from .latest_publications import latest_publications
 from django.shortcuts import render, get_object_or_404
+from .latest_publications import latest_publications
 from .models import Blog, CatalogOfArticles
 from .get_blogs_in_category import *
-   
+
+
 def index(request):
     return render(request, 'index.html', {
         'catalogs': count_blog_entries_by_catalog(),
@@ -12,31 +13,14 @@ def index(request):
         })
 
 def blog_detail(request, slug):
-    blog = get_object_or_404(Blog, slug=slug) 
+    blog = get_object_or_404(Blog, slug=slug)
+    catalog = get_object_or_404(CatalogOfArticles, title=blog.catalog_name)
     return render(request, 'blog_detail.html', {
+        'catalog': catalog,
         'blog': blog,
         'catalogs': count_blog_entries_by_catalog(),
         'latest_publications': latest_publications(),
         })
-
-# def catalog_name(request, pk):
-#     blog = get_blogs_in_category()    
-#     blogs = get_object_or_404(CatalogOfArticles, pk=pk)
-#     return render(request, 'category.html', {
-#         'blogs': blog[blogs.title],
-#         'catalogs': count_blog_entries_by_catalog(),
-#         'latest_publications': latest_publications(),
-#         })
-
-from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from .catalog_entry_counts import count_blog_entries_by_catalog  
-from .latest_publications import latest_publications
-from .models import Blog, CatalogOfArticles
-
-
-
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def catalog_name(request, slug):
     catalog = get_object_or_404(CatalogOfArticles, slug=slug)
