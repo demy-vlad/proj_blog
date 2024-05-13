b9bfdda81597b43be8758324b1bba13d4d3b7ffc
Vlad <you@example.com>
2024-05-13 22:10:39 +0300

    fix

--- blog/main/get_blogs_in_category.py
+++ blog/main/get_blogs_in_category.py
@@ -1,15 +1,16 @@
-from collections import defaultdict
 from .models import CatalogOfArticles, Blog
 
 def get_blogs():
-    all_blogs_in_category = defaultdict(list)
+    all_blogs_in_category = {}
     for category in CatalogOfArticles.objects.all():
         all_blogs_in_category[category.title] = []
 
     for blogs in Blog.objects.all():
-        all_blogs_in_category[blogs.catalog_name].append(blogs)
+        if blogs.catalog_name in all_blogs_in_category:
+            all_blogs_in_category[blogs.catalog_name].append(blogs)
         
     return all_blogs_in_category
 
 def get_blogs_in_category(category_name):
-    return Blog.objects.filter(catalog_name=category_name)
+    blogs_in_category = Blog.objects.filter(catalog_name=category_name)
+    return blogs_in_category
--- blog/main/views.py
+++ blog/main/views.py
@@ -1,38 +1,48 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .catalog_entry_counts import count_blog_entries_by_catalog  
 from django.shortcuts import render, get_object_or_404
+from .latest_publications import latest_publications
 from .models import Blog, CatalogOfArticles
-from django.db.models import Count
+from .get_blogs_in_category import *
+import markdown
 
 def index(request):
-    catalogs = CatalogOfArticles.objects.annotate(blog_count=Count('blog'))
-    latest_publications = Blog.objects.order_by('-created_at')[:10]
     return render(request, 'index.html', {
-        'catalogs': catalogs,
-        'latest_publications': latest_publications,
+        'catalogs': count_blog_entries_by_catalog(),
+        'latest_publications': latest_publications(),
         })
 
 def blog_detail(request, slug):
     blog = get_object_or_404(Blog, slug=slug)
     catalog = get_object_or_404(CatalogOfArticles, title=blog.catalog_name)
+    
     return render(request, 'blog_detail.html', {
         'catalog': catalog,
         'blog': blog,
-    })
+        'full_description': markdown.markdown(blog.full_description),
+        'catalogs': count_blog_entries_by_catalog(),
+        'latest_publications': latest_publications(),
+        })
 
 def catalog_name(request, slug):
     catalog = get_object_or_404(CatalogOfArticles, slug=slug)
-    blogs = Blog.objects.filter(catalog_name=catalog.title)
+    blogs = get_blogs_in_category(catalog.title)  # Отримуємо блоги з даної категорії
 
-    paginator = Paginator(blogs, 30)
+    # Застосовуємо пагінацію
+    paginator = Paginator(blogs, 30)  # Показуємо 10 блогів на сторінці
     page = request.GET.get('page')
     try:
         blogs_page = paginator.page(page)
     except PageNotAnInteger:
+        # Якщо параметр сторінки не є цілим числом, показуємо першу сторінку
         blogs_page = paginator.page(1)
     except EmptyPage:
+        # Якщо параметр сторінки виходить за межі, показуємо останню сторінку
         blogs_page = paginator.page(paginator.num_pages)
         
     return render(request, 'category.html', {
         'catalog': catalog,
-        'blogs': blogs_page,
+        'blogs': blogs_page,  # Передаємо лише сторінку блогів, а не всі блоги
+        'catalogs': count_blog_entries_by_catalog(),
+        'latest_publications': latest_publications(),
     })
